2df41ae5bcc8556dfa4fc32b55631ddb5601144b
zogak <zogak7@gmail.com>
2021-11-23 22:59:45 +0900

    feat : Add list approach

---
+++ about_python/list.py
@@ -0,0 +1,13 @@
+'''
+list에 대해 정리합니다.
+'''
+
+a = [1, 2, 3]
+b = ['apple', 'orange', 'grape']
+
+# list의 element에 접근하는 법
+print(a[0], a[1], a[2])
+
+# list의 element의 index를 알고싶은 경우
+print(a.index(1))
+print(b.index('grape'))
