c70e50878ae57921507f5c113c5c87003ae6647d
zogak <zogak7@gmail.com>
2021-12-01 00:15:11 +0900

    feat : Add tkinter

---
+++ about_tkinter/tkinter1.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+# create window object
+win = Tk()
+
+# set window size
+win.geometry("600x400")
+
+# set window title
+win.title("zogak")
+
+# start window
+win.mainloop()
